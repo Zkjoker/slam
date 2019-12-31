4ab122876e5308617daf76de4a76e42cb1382fd5
zhaokai <zkyy828@163.com>
2019-12-31 11:08:34 +0800

    project

---
+++ docker_set.sh
@@ -0,0 +1,7 @@
+docker pull zkjoker/image1:19-12-30
+docker pull zkjoker/image2:19-12-31
+docker pull zkjoker/image3:19-12-30
+docker pull zkjoker/image4:v1
+docker pull zkjoker/image5:v2
+docker pull zkjoker/image6:rgbd
+docker pull zkjoker/laserslam:v4
